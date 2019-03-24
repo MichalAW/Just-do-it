32837c12f58b747b5bd04169892f04aaab00aaf8
MichalAW <michal.wanta@gmail.com>
2019-03-24 16:50:10 +0100

    css,html,js changes

--- js/javascript.js
+++ js/javascript.js
@@ -1,41 +1,17 @@
-document.addEventListener("DOMContentLoaded", ready);
-  function ready() {
-    var toggle = document.getElementById("toggle");
-
-new Chart(document.getElementById("bar-chart"), {
-    type: 'bar',
-    data: {
-      labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-      datasets: [
-        {
-          label: "Signups",
-          backgroundColor: "#56819F",
-          data: [133,221,283,478,576,567,444,414,266,166]
-        }, {
-          label: "FTD",
-          backgroundColor: "#FFA200",
-          data: [208,147,375,134,565,555,237,444,334,276]
-        }
-      ]
-    },
-    options: {
-      title: {
-        display: true,
-      }
-    }
-});
+// toggle menu sidebar
+var toggle = document.getElementById("toggle");
+// obsluga sidebara
+var links = document.querySelectorAll('.sidebar a');
+var sections = document.querySelectorAll('main section');
 
 function toggleSidebar(){
   document.getElementById('sidebar').classList.toggle('active');
+  document.querySelector('main').classList.toggle('fullwidth');
 }
 
 toggle.addEventListener("click", function(){
   toggleSidebar();
 });
-}; 
-// obsluga sidebara
-var links = document.querySelectorAll('.sidebar a');
-var sections = document.querySelectorAll('main section');
 
 // nadanie eventu na wszystkich linkach z sidebara
 for (var i = 0; i < links.length; i++) {
@@ -63,6 +39,32 @@ function refreshDate() {
 
     // }
   }
-}+
+}
+
+function ready() {
+  new Chart(document.getElementById("bar-chart"), {
+      type: 'bar',
+      data: {
+        labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+        datasets: [
+          {
+            label: "Signups",
+            backgroundColor: "#56819F",
+            data: [133,221,283,478,576,567,444,414,266,166]
+          }, {
+            label: "FTD",
+            backgroundColor: "#FFA200",
+            data: [208,147,375,134,565,555,237,444,334,276]
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+        }
+      }
+  });
+};
 
 document.querySelector('.btn-details').addEventListener('click', refreshDate);
+document.addEventListener("DOMContentLoaded", ready);
