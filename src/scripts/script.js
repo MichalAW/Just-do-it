14f88591d6fcaf4e5da771d862c9937decfe0a7a
MichalAW <michal.wantaeuco@gmail.com>
2019-08-02 15:12:28 +0200

    fix rwd sidebar and data

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -81,38 +81,15 @@ if (button) {
 
 document.addEventListener("DOMContentLoaded", ready);
 
-//modal
+//modal jquery
 
-(function() {
-  var showModal = function(event) {
-    event.preventDefault();
-    document.querySelector("#modal-overlay").classList.add("show");
-  };
-
-  var modalLinks = document.querySelectorAll(".show-modal");
-
-  for (var i = 0; i < modalLinks.length; i++) {
-    modalLinks[i].addEventListener("click", showModal);
-  }
-
-  var hideModal = function(event) {
-    event.preventDefault();
-    document.querySelector("#modal-overlay").classList.remove("show");
-  };
-
-  var closeButtons = document.querySelectorAll(".modal .close");
-
-  for (var i = 0; i < closeButtons.length; i++) {
-    closeButtons[i].addEventListener("click", hideModal);
-  }
-
-  document.querySelector("#modal-overlay").addEventListener("click", hideModal);
-
-  var modals = document.querySelectorAll(".modal");
-
-  for (var i = 0; i < modals.length; i++) {
-    modals[i].addEventListener("click", function(event) {
-      event.stopPropagation();
-    });
-  }
-})();
+$(document).ready(function() {
+  $(".modal-link").click(function() {
+    $(".modal").show();
+    $(".modal-bg").show();
+  });
+  $(".modal .close").click(function() {
+    $(".modal").hide();
+    $(".modal-bg").hide();
+  });
+});
